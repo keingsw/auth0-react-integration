891792afb20d5a360accd439615e3f7122634348
keingsw <dev.keingsw@gmail.com>
2020-03-23 15:52:01 +0800

    feat(auth): implement `handleAuthentication()` to Auth0 class

--- src/Auth/auth0.js
+++ src/Auth/auth0.js
@@ -16,4 +16,28 @@ export default class Auth0 {
     console.debug('[Auth0] login');
     this.webAuth.authorize();
   }
+
+  handleAuthentication() {
+    console.debug('[Auth0] received auth callback');
+
+    return new Promise((resolve, reject) => {
+      this.webAuth.parseHash((authError, authResult) => {
+        if (authError || !authResult.idToken || !authResult.accessToken) {
+          console.error('[Auth0] error logging in', authError);
+          reject(authError);
+        }
+
+        console.debug('[Auth0] logged in successfully');
+        this.setSession(authResult);
+        resolve(authResult);
+      });
+    });
+  }
+
+  setSession({ accessToken, idTokenPayload, expiresIn }) {
+    console.debug(`[Auth0] token expires in ${expiresIn} seconds`);
+    this.accessToken = accessToken;
+    this.userInfo = idTokenPayload;
+    this.expiresAt = expiresIn * 1000 + new Date().getTime();
+  }
 }
--- src/Auth/auth0.test.js
+++ src/Auth/auth0.test.js
@@ -12,10 +12,30 @@ describe('Auth0', () => {
   const redirectUri = 'http://localhost:3000/auth/callback';
   const scope = 'openid profile email';
 
+  const successfulAuthError = null;
+  const successfulAuthResult = {
+    accessToken: 'abc',
+    idToken: 'def',
+    idTokenPayload: {
+      email: 'test@example.com',
+      email_verified: true,
+    },
+    expiresIn: 24 * 60 * 60,
+  };
+
+  const failedAuthError = { error: 'Something went wrong' };
+  const failedAuthResult = {};
+
   const mockWebAuth = WebAuth;
 
   const initializeAuth0 = () => {
     auth0 = new Auth0({ audience, domain, clientId, redirectUri, scope });
+  };
+
+  const mockParseHashImplementation = (error, result) => {
+    return cb => {
+      cb(error, result);
+    };
   };
 
   beforeEach(() => {
@@ -50,6 +70,110 @@ describe('Auth0', () => {
     test('initiates authrozation redirect with auth0', () => {
       auth0.login();
       expect(mockWebAuth.mock.instances[0].authorize).toHaveBeenCalledWith();
+    });
+  });
+
+  describe('handleAuthentication()', () => {
+    test('logs a debug message', () => {
+      auth0.handleAuthentication();
+      expect(console.debug).toHaveBeenCalledWith(
+        '[Auth0] received auth callback'
+      );
+    });
+    test('parses the hash', () => {
+      auth0.handleAuthentication();
+      expect(mockWebAuth.mock.instances[0].parseHash).toHaveBeenCalled();
+    });
+
+    describe('on success', () => {
+      beforeEach(() => {
+        mockWebAuth.mockImplementationOnce(() => {
+          return {
+            parseHash: mockParseHashImplementation(
+              successfulAuthError,
+              successfulAuthResult
+            ),
+          };
+        });
+        initializeAuth0();
+      });
+
+      test('logs a debug message', async () => {
+        expect.assertions(1);
+        await auth0.handleAuthentication();
+        expect(console.debug).toHaveBeenCalledWith(
+          '[Auth0] logged in successfully'
+        );
+      });
+
+      test('sets accessToken', async () => {
+        expect.assertions(1);
+        await auth0.handleAuthentication();
+        expect(auth0.accessToken).toBe('abc');
+      });
+
+      test('calculates and sets expiresAt', async () => {
+        expect.assertions(1);
+        await auth0.handleAuthentication();
+        expect(auth0.expiresAt).toBeTruthy();
+      });
+
+      test('logs a debug message with the token expiry time', async () => {
+        expect.assertions(1);
+        await auth0.handleAuthentication();
+        expect(console.debug).toHaveBeenCalledWith(
+          '[Auth0] token expires in 86400 seconds'
+        );
+      });
+
+      test('sets userInfo', async () => {
+        expect.assertions(1);
+        await auth0.handleAuthentication();
+        expect(auth0.userInfo).toEqual({
+          email: 'test@example.com',
+          email_verified: true,
+        });
+      });
+
+      test('resolves and provides the auth result', async () => {
+        expect.assertions(1);
+        await expect(auth0.handleAuthentication()).resolves.toBe(
+          successfulAuthResult
+        );
+      });
+    });
+
+    describe('on error', () => {
+      beforeEach(() => {
+        mockWebAuth.mockImplementationOnce(() => {
+          return {
+            parseHash: mockParseHashImplementation(
+              failedAuthError,
+              failedAuthResult
+            ),
+          };
+        });
+        initializeAuth0();
+      });
+
+      test('rejects and provides the auth error', async () => {
+        expect.assertions(1);
+        await expect(auth0.handleAuthentication()).rejects.toBe(
+          failedAuthError
+        );
+      });
+
+      test('logs an error', async () => {
+        expect.assertions(1);
+        try {
+          await auth0.handleAuthentication();
+        } catch (error) {
+          expect(console.error).toHaveBeenCalledWith(
+            '[Auth0] error logging in',
+            failedAuthError
+          );
+        }
+      });
     });
   });
 });
