bd998d1931a5a1d90b4c30e02f70346fc0956737
keingsw <dev.keingsw@gmail.com>
2020-03-23 15:53:25 +0800

    feat(auth): implement `renewSession()` to Auth0 class

--- src/Auth/auth0.js
+++ src/Auth/auth0.js
@@ -34,6 +34,23 @@ export default class Auth0 {
     });
   }
 
+  renewSession() {
+    console.debug('[Auth0] renew session');
+
+    return new Promise((resolve, reject) => {
+      this.webAuth.checkSession({}, (authError, authResult) => {
+        if (authError || !authResult.idToken || !authResult.accessToken) {
+          console.error('[Auth0] error renewing session', authError);
+          reject(authError);
+        }
+
+        console.debug('[Auth0] renewed session successfully');
+        this.setSession(authResult);
+        resolve(authResult);
+      });
+    });
+  }
+
   setSession({ accessToken, idTokenPayload, expiresIn }) {
     console.debug(`[Auth0] token expires in ${expiresIn} seconds`);
     this.accessToken = accessToken;
--- src/Auth/auth0.test.js
+++ src/Auth/auth0.test.js
@@ -176,4 +176,109 @@ describe('Auth0', () => {
       });
     });
   });
+
+  describe('renewSession', () => {
+    const mockCheckSessionImplementation = (authError, authResult) => {
+      return (o, cb) => {
+        cb(authError, authResult);
+      };
+    };
+
+    test('checks the session', () => {
+      auth0.renewSession();
+      expect(mockWebAuth.mock.instances[0].checkSession).toHaveBeenCalled();
+    });
+    test('logs debug message', () => {
+      auth0.renewSession();
+      expect(console.debug).toHaveBeenCalledWith('[Auth0] renew session');
+    });
+
+    describe('on success', () => {
+      beforeEach(() => {
+        mockWebAuth.mockImplementationOnce(() => {
+          return {
+            checkSession: mockCheckSessionImplementation(
+              successfulAuthError,
+              successfulAuthResult
+            ),
+          };
+        });
+        initializeAuth0();
+      });
+
+      test('logs debug message', async () => {
+        expect.assertions(1);
+        await auth0.renewSession();
+        expect(console.debug).toHaveBeenCalledWith(
+          '[Auth0] renewed session successfully'
+        );
+      });
+
+      test('sets accessToken', async () => {
+        expect.assertions(1);
+        await auth0.renewSession();
+        expect(auth0.accessToken).toBe('abc');
+      });
+
+      test('calculates and sets expiresAt', async () => {
+        expect.assertions(2);
+        expect(auth0.expiresAt).toBe(undefined);
+        await auth0.renewSession();
+        expect(auth0.expiresAt).toBeTruthy();
+      });
+
+      test('logs debug message with the token expiry time', async () => {
+        expect.assertions(1);
+        await auth0.renewSession();
+        expect(console.debug).toHaveBeenCalledWith(
+          '[Auth0] token expires in 86400 seconds'
+        );
+      });
+
+      test('sets userInfo', async () => {
+        expect.assertions(1);
+        await auth0.renewSession();
+        expect(auth0.userInfo).toEqual({
+          email: 'test@example.com',
+          email_verified: true,
+        });
+      });
+
+      test('resolves and provides the auth result', async () => {
+        expect.assertions(1);
+        await expect(auth0.renewSession()).resolves.toBe(successfulAuthResult);
+      });
+    });
+
+    describe('on error', () => {
+      beforeEach(() => {
+        mockWebAuth.mockImplementationOnce(() => {
+          return {
+            checkSession: mockCheckSessionImplementation(
+              failedAuthError,
+              failedAuthResult
+            ),
+          };
+        });
+        initializeAuth0();
+      });
+
+      test('rejects adn provides the auth error', async () => {
+        expect.assertions(1);
+        await expect(auth0.renewSession()).rejects.toBe(failedAuthError);
+      });
+
+      test('logs an error', async () => {
+        expect.assertions(1);
+        try {
+          await auth0.renewSession();
+        } catch (error) {
+          expect(console.error).toHaveBeenCalledWith(
+            '[Auth0] error renewing session',
+            failedAuthError
+          );
+        }
+      });
+    });
+  });
 });
